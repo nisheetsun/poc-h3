62fb5aa902758f4c23a60cc36a494ca29f7ecd70
nisheet Verma <nisheetverma@nisheets-MacBook-Pro.local>
2021-03-06 21:10:13 +0530

    docker init

--- backend/routes/bikes.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-const { bikes } = require('../controllers/bikesControllers');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', bikes);
-
-module.exports = router;
--- backend/services/geo.js
+++
@@ -1,5 +0,0 @@
-const h3 = require("h3-js");
-
-exports.lat_long_to_index = function(lat, long, resolution=7){
-    return h3.geoToH3(lat, long, resolution)
-}
